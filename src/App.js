ee127b7ed16f14e60b1bb195e6139e6afb581415
Francis Inegbenose <kizzycole10@gmail.com>
2024-03-23 13:55:36 +0100

    setup basic html, css and installed leaflet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="firstContainer">
+        <div className="">
+          <h1>IP Address Tracker</h1>
+          <div class="formbox">
+            <form action="#" class="form_input">
+              <input
+                type="text"
+                class="input_field"
+                placeholder="Search for any IP address or domain"
+              />
+              <button class="input_btn">
+                <img src="/images/icon-arrow.svg" alt="icon" />
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="secondContainer">
+        <div className="secondContainerDetail">
+          <span>IP ADDRESS</span>
+          <h2>192.212.174.101</h2>
+        </div>
+        <div className="secondContainerDetail">
+          <span>LOCATION</span>
+          <h2>Brooklyn, NY 10001</h2>
+        </div>
+        <div className="secondContainerDetail">
+          <span>TIMEZONE</span>
+          <h2>UTC -05:00</h2>
+        </div>
+        <div className="secondContainerDetail">
+          <span>ISP</span>
+          <h2> SpaceX Starlink</h2>
+        </div>
+      </div>
+      <div className="thirdContainer"></div>
     </div>
   );
 }
